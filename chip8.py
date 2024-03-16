ff3d06a66624c0be16da83ef96171671b979a2d2
waewing <waewing24@gmail.com>
2024-03-16 19:12:24 -0400

    Update chip8.py

--- chip8.py
+++ chip8.py
@@ -1,5 +1,9 @@
 import logging
 import pygame
+import time
+import sys
+import random
+import os
 
 #Chip 8 Specifications
 #---------------------------------------------------------------------
@@ -18,6 +22,182 @@ import pygame
 
 #---------------------------------------------------------------------
 
+
+#Emulator Architecture
+
+class Emulator:
+  def __init__(self):
+    pygame.display.flip()
+
+  def opCodes(self,opcode):
+
+    pos1 = opcode[0]
+    pos3 = opcode[2]
+    pos4 = opcode[3]
+
+    match pos1:
+      case '0':
+        #00E0 and 00EE
+
+        match pos4:
+           case '0':
+            #00E0 - clear the display
+            return
+           
+           case 'e':
+            #00EE - return from a subroutine, set pc to top of stack then subtract 1 from stack pointer
+            return
+          
+      case '1':
+        #1nnn - jump to location at nnn, set pc to nnn
+        return 
+      
+      case '2':
+        #2nnn - call subroutine at nnn, increments the stack pointer, puts the current PC on top of the stack ,sets PC to nnn.
+        return
+      
+      case '3':
+        #3xkk - skip the next instruction if Vx = kk, checks if Vx = kk if so increment PC by 2
+        return
+      
+      case '4':
+        #4xkk - skip the next instruction if Vx != kk, checks if Vx != kk if so increment PC by 2
+        return
+      
+      case '5':
+        #5xy0 - skip the next instruction if Vx = Vy, checks if Vx = Vy if so increment PC by 2
+        return
+      
+      case '6':
+        #6xkk - puts the value kk in register Vx
+        return
+
+      case '7':
+        #7xkk - adds value kk to value in register Vx, stores it in Vx
+        return
+
+      case '8': 
+        #8xy0 8xy1 8xy2 8xy3 8xy4 8xy5 8xy6 8xy7 8xyE
+
+        match pos4:
+          case '0':
+            #8xy0 - Set Vx = Vy, stores the value of register Vy in register Vx
+            return
+
+          case '1':
+            #8xy1 - Set Vx = Vx OR Vy, Performs a bitwise OR on the values of Vx and Vy, then stores the result in Vx
+            return
+          
+          case '2':
+            #8xy2 - Set Vx = Vx AND Vy, Performs a bitwise AND on the values of Vx and Vy, then stores the result in Vx
+            return
+
+          case '3':
+            #8xy3 - Set Vx = Vx XOR Vy, Performs a bitwise exclusive OR on the values of Vx and Vy, then stores the result in Vx
+            return
+          
+          case '4':
+            #8xy4 - Set Vx = Vx + Vy, set VF = carry, The values of Vx and Vy are added together If the result is greater than 8 bits (i.e., > 255,) VF is set to 1, otherwise 0 Only the lowest 8 bits of the result are kept, and stored in Vx
+            return
+
+          case '5':
+            #8xy5 - Set Vx = Vx - Vy, set VF = NOT borrow, If Vx > Vy, then VF is set to 1, otherwise 0 Then Vy is subtracted from Vx, and the results stored in Vx
+            return
+          
+          case '6':
+            #8xy6 - Set Vx = Vx SHR 1, If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is divided by 2.
+            return
+
+          case '7':
+            #8xy7 - Set Vx = Vy - Vx, set VF = NOT borrow, If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is subtracted from Vy, and the results stored in Vx.
+            return
+
+          case 'e':
+            #8xyE - Set Vx = Vx SHL 1, If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2.
+            return
+
+      case '9':
+        #9xy0 - Skip next instruction if Vx != Vy, The values of Vx and Vy are compared, and if they are not equal, the program counter is increased by 2.
+        return 
+      
+      case 'a':
+        #Annn - Set I = nnn, The value of register I is set to nnn
+        return
+
+      case 'b':
+        #Bnnn - Jump to location nnn + V0, The program counter is set to nnn plus the value of V0
+        return
+      
+      case 'c':
+        #Cxkk - Set Vx = random byte AND kk, The interpreter generates a random number from 0 to 255, which is then ANDed with the value kk. The results are stored in Vx. See instruction 8xy2 for more information on AND 
+        return
+      
+      case 'd':
+        #Dxyn - Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
+        return
+
+      case 'e':
+        #Ex9E ExA1
+
+        match pos4:
+
+          case 'e':
+            #Ex9E - Skip next instruction if key with the value of Vx is pressed
+            return
+          
+          case '1':
+            #ExA1 - Skip next instruction if key with the value of Vx is not pressed
+            return
+
+      case 'f':
+        #Fx07 Fx0A Fx18 Fx1E Fx29 Fx33 Fx15 Fx55 Fx65
+
+          match pos4:
+
+            case '7':
+              #Fx07 - Set Vx = delay timer value
+              return
+            
+            case 'a':
+              #Fx0A - Wait for a key press, store the value of the key in Vx.
+              return
+            
+            case '8':
+              #Fx18 - Set sound timer = Vx
+              return
+            
+            case 'e':
+              #Fx18 - Set I = I + Vx
+              return
+            
+            case '9':
+              #Fx29 - Set I = location of sprite for digit Vx
+              return
+            
+            case '3':
+              #Fx33 - Store BCD representation of Vx in memory locations I, I+1, and I+2
+              return
+            
+            case '5':
+              #Fx15 Fx55 Fx65
+
+              match pos3:
+
+                case '1':
+                  #Fx15 - Set delay timer = Vx
+                  return
+            
+                case '5':
+                  #Fx55 - Store registers V0 through Vx in memory starting at location I
+                  return
+                
+                case '6':
+                  #Fx65 - Read registers V0 through Vx from memory starting at location I
+                  return
+
+      case _:
+        print("Invalid OpCode")
+      
 #Initiliaze components
 def initialize(self):
   self.clear()
